8eee2b21ca9cd8393854c7391fd39547504f6776
Sugizou0215 <sugizou0215@gmail.com>
2021-08-13 09:46:44 +0000

    [Update]7-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,11 @@ class BooksController < ApplicationController
   end
 
   def create
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
     else
       @books = Book.all
       render action: :index
@@ -22,6 +23,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    user_id = Book.find(params[:id]).user_id
+    @user = User.find(user_id) #投稿したユーザのidを取得
   end
 
   def edit
