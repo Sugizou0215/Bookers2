b425347ae9f12a5e9e869b8e28a935e08474360a
Sugizou0215 <sugizou0215@gmail.com>
2021-08-21 02:42:03 +0000

    [Start]2-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   has_many :favorites, dependent: :destroy
+  
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :followed_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
+  has_many :followers, through: :reverse_of_relationships, source: :follower_id
 
 	#バリデーション
 	validates :name, length: {in: 2..20}, uniqueness: true
