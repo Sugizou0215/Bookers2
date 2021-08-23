05d16ce85d43a9e6d7c61d96d38dec7879c35c9b
Sugizou0215 <sugizou0215@gmail.com>
2021-08-23 01:51:14 +0000

    [Finish]2-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,18 @@
 class PostCommentsController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
+		@book = Book.find(params[:book_id])
     @post_comment = PostComment.new(post_comment_params)
 		@post_comment.user_id = current_user.id
-    @post_comment.book_id = book.id
+    @post_comment.book_id = @book.id
     if @post_comment.save
-    	redirect_to book_path(book), notice: 'You have updated comment successfully.'
     else
 		  render 'books/show'
 		end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
   	PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
